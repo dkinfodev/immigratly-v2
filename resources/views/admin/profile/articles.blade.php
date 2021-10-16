2ca7e217151ab0b0b740fd35d3ce6706f29e5882
devilyash1 <coderworld001@gmail.com>
2021-10-16 21:51:59 +0530

    changes done

--- resources/views/admin/profile/articles.blade.php
+++ resources/views/admin/profile/articles.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.master')
-
+@section('pageheader')
+<!-- Content -->
+<div class="">
+    <div class="content container" style="height: 25rem;">
+        <!-- Page Header -->
+        <div class="page-header page-header-light page-header-reset">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h1 class="page-header-title">{{$pageTitle}}</h1>
+                </div>
+            </div>
+            <!-- End Row -->
+        </div>
+        <!-- End Page Header -->
+    </div>
+</div> 
+<!-- End Content -->
+@endsection
 @section('content')
 
 <!-- Content -->
-      <div class="content container-fluid">
+      <div class="articles">
         <div class="row justify-content-lg-center">
           <div class="col-lg-10">
             
